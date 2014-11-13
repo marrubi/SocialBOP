f89646cd3e96ec91104f963588a88762247a1190
marrubi <mar.arraubi@gmail.com>
2014-11-12 22:07:17 -0300

    Versión Azul-Celeste retocada de la aplicación
    Se sube, debido al impacto visual que provoca la versión
    Amarilla-Pistacho

---
+++ SocialBOP/src/com/example/socialbop/ActivityRecPass.java
@@ -0,0 +1,42 @@
+package com.example.socialbop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityRecPass extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recpass);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity6, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		switch (id) {
+			case android.R.id.home:
+				finish();
+				break;
+			case R.id.action_enviar:
+				finish();
+				break;
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
